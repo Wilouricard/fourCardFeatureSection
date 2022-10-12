752f3dd3683f488cda0fbd8c94320ab90292f7b5
Nam Tang <nam.tang@live.fr>
2022-10-12 13:15:30 +0200

    final version desktop v.added

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,64 @@ function App() {
   return (
     <main className="flex flex-col mx-10 my-10">
       <Header />
-      {cards}
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col bg-white mb-8 p-6 rounded-lg border-t-4 border-Cyan shadow-xl hover:border-Red lg:max-w-[325px] max-h-[233px] lg:mr-8 lg:mt-32">
+          <h1 className="font-semibold text-Very-Dark-Blue pb-2 text-lg">
+            Supervisor
+          </h1>
+          <p className="text-sm text-Grayish-Blue pb-12">
+            Monitors activity to identify project roadblocks
+          </p>
+          <img
+            src="../images/icon-supervisor.svg"
+            alt=""
+            className="w-14 self-end"
+          />
+        </div>
+        <div className="flex flex-col">
+          <div className="flex flex-col bg-white mb-8 p-6 rounded-lg border-t-4 border-Red shadow-xl hover:border-Red lg:max-w-[325px] max-h-[233px]">
+            <h1 className="font-semibold text-Very-Dark-Blue pb-2 text-lg">
+              Team Builder
+            </h1>
+            <p className="text-sm text-Grayish-Blue pb-12">
+              {" "}
+              Scans our talent network to create the optimal team for your
+              project
+            </p>
+            <img
+              src="../images/icon-team-builder.svg"
+              alt=""
+              className="w-14 self-end"
+            />
+          </div>
+          <div className="flex flex-col bg-white mb-8 p-6 rounded-lg border-t-4 border-y-Orange shadow-xl hover:border-Red lg:max-w-[325px] max-h-[233px]">
+            <h1 className="font-semibold text-Very-Dark-Blue pb-2 text-lg">
+              Karma
+            </h1>
+            <p className="text-sm text-Grayish-Blue pb-12">
+              Regularly evaluates our talent to ensure quality
+            </p>
+            <img
+              src="../images/icon-karma.svg"
+              alt=""
+              className="w-14 self-end"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col bg-white mb-8 p-6 rounded-lg border-t-4 border-Blue shadow-xl hover:border-Red lg:max-w-[325px] max-h-[233px] lg:ml-8 lg:mt-32">
+          <h1 className="font-semibold text-Very-Dark-Blue pb-2 text-lg">
+            Calculator
+          </h1>
+          <p className="text-sm text-Grayish-Blue pb-12">
+            Uses data from past projects to provide better delivery estimates
+          </p>
+          <img
+            src="../images/icon-calculator.svg"
+            alt=""
+            className="w-14 self-end"
+          />
+        </div>
+      </div>
     </main>
   );
 }
